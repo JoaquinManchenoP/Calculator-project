737c6a10ad8b79d1c7018ef8e76d5683266e4b00
JoaquinManchenoP <manchenojj@g.cofc.edu>
2021-04-23 20:07:22 -0400

    styles

--- app/src/main/java/com/example/calcultor_mancheno/MainActivity.java
+++ app/src/main/java/com/example/calcultor_mancheno/MainActivity.java
@@ -9,26 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button cButton;
-    Button plusDivideSubtract;
-    Button ceButton;
-    Button divide;
-    Button add;
-    Button subtract;
-    Button multiply;
-    Button period;
-    Button equal;
-    Button number0;
-    Button number1;
-    Button number2;
-    Button number3;
-    Button number4;
-    Button number5;
-    Button number6;
-    Button number7;
-    Button number8;
-    Button number9;
-    TextView resultView;
+
 
 
 
@@ -38,143 +19,75 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cButton = (Button)findViewById(R.id.cButton);
-
-
-
-
-        plusDivideSubtract = (Button)findViewById(R.id.plusDivideSubtract);
-        ceButton = (Button)findViewById(R.id.ceButton);
-        divide = (Button)findViewById(R.id.divideButton);
-        add = (Button)findViewById(R.id.addButton);
-
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                resultView.setText(resultView.getText().toString() + "+");
-
-
-
-
-            }
-        });
-
-
-
-        subtract = (Button)findViewById(R.id.subtractButton);
-        multiply = (Button)findViewById(R.id.multiplyButton);
-        period = (Button)findViewById(R.id.periodButton);
-        equal = (Button)findViewById(R.id.equalButton);
-
-        resultView = (TextView) findViewById(R.id.resultView);
-
-
-
-
-
-
-
-        number0 = (Button)findViewById(R.id.zeroButton);
-        number0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                    resultView.setText(resultView.getText().toString() + "0");
-
-
-            }
-        });
-
-
-
-        number1= (Button)findViewById(R.id.oneButton);
-        number1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resultView.setText(resultView.getText().toString() + "1");
-            }
-        });
-
-
-        number2 = (Button)findViewById(R.id.twoButton);
-        number2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resultView.setText(resultView.getText().toString() + "2");
-            }
-        });
-
-
-        number3 = (Button)findViewById(R.id.threeButton);
-        number3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resultView.setText(resultView.getText().toString() + "3");
-            }
-        });
-
-        number4 = (Button)findViewById(R.id.fourButton);
-        number4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resultView.setText(resultView.getText().toString() + "4");
-            }
-        });
-
-        number5 = (Button)findViewById(R.id.fiveButton);
-        number5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resultView.setText(resultView.getText().toString() + "5");
-            }
-        });
-
-        number6 = (Button)findViewById(R.id.sixButton);
-        number6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resultView.setText(resultView.getText().toString() + "6");
-            }
-        });
-
-        number7 = (Button)findViewById(R.id.sevenButton);
-        number7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resultView.setText(resultView.getText().toString() + "7");
-            }
-        });
-
-        number8 = (Button)findViewById(R.id.eightButton);
-        number8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                 resultView.setText(resultView.getText().toString() + "8");
-            }
-        });
-
-        number9 = (Button)findViewById(R.id.nineButton);
-        number9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resultView.setText(resultView.getText().toString() + "0");
-            }
-        });
-
-        ceButton = (Button)findViewById(R.id.ceButton);
-        ceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resultView.setText("0");
-            }
-        });
-        cButton = (Button)findViewById(R.id.cButton);
-        cButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resultView.setText("0");
-            }
-        });
     }
+
+
+    public void sevenButton(View view){
+
+    }
+
+    public void eightButton(View view){
+
+    }
+
+    public void nineButton(View view){
+
+    }
+
+    public void divideButton(View view){
+
+    }
+
+    public void fourButton(View view){
+
+    }
+
+    public void fiveButton(View view){
+
+    }
+
+    public void sixButton(View view){
+
+    }
+
+    public void multiplyButton(View view){
+
+    }
+
+    public void oneButton(View view){
+
+    }
+
+    public void twoButton(View view){
+
+    }
+
+    public void minusButton(View view){
+
+    }
+
+    public void threeButton(View view){
+
+    }
+
+    public void clearButton(View view){
+
+    }
+
+    public void zeroButton(View view){
+
+    }
+
+    public void periodButton(View view){
+
+    }
+
+    public void plusButton(View view){
+
+    }
+
+    public void equalButton(View view){
+
+    }
+
 }
